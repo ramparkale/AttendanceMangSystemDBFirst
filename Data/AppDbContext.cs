f8084ec3b1bd565a67c0a8326dd2c682ffad40e1
ramparkale <parkaleram@gmail.com>
2026-09-03 20:07:13 +0530

    ok

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -25,12 +25,11 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=DESKTOP-41O9E62;Database=AttendanceDBDatabaseFirst;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Attendance>(entity =>
+        modelBuilder.Entity<Attendance>(entity => 
         {
             entity.HasKey(e => e.AttendanceId).HasName("PK__Attendan__8B69261CDAB3EE06");
 
